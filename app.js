313a3c646574d2e731170699edf9116328bb03c4
ilyt <a@ilyt.pw>
2018-05-17 23:32:56 +0100

    add version

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const version = "0.01-beta"
 
 app.post("/", require("./handler/main"));
-app.listen(80, () => console.log("le very basic osu bancho v0.0.0.1 autism edition"));
+app.listen(80, () => console.log("levbob v" + version));
