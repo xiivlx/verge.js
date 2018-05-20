dd2f029c1e605d70a563c75cfd8af6b3b9a6487f
ilyt <38846239+ilyt@users.noreply.github.com>
2018-05-20 23:28:05 +0100

    i heard nothing

--- verge.js
+++ verge.js
@@ -1,6 +1,9 @@
 const app = require("express")();
 const console_helper = require("./helpers/console");
 const config = require("./config");
+const stream = require("../helpers/stream");
+
+stream.add_stream("main");
 
 console_helper.print_metadata();
 
