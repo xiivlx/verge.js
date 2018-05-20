e9205ae9b375f694a413750f244a1ab1a341fb60
ilyt <38846239+ilyt@users.noreply.github.com>
2018-05-20 18:45:05 +0100

    better config
    thanks ggms

--- config.js
+++ config.js
@@ -1,3 +1,22 @@
 module.exports = {
-    port: 80
+    server: {
+        port: 80,
+        apikey: '',
+        choprotocolversion: 19,
+        debug: false
+    },
+    Bancho: {
+        ServerName: 'Verge',
+        BotName: 'Mitsuha',
+        SupportEmail: 'support@verge.moe',
+        freedirect: true
+    },
+    mysql: {
+        pool: 128,
+        host: '127.0.0.1',
+        port: 3306,
+        username: 'root',
+        password: '',
+        database: 'verge'
+    }
 };
