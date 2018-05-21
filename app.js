b0c64370309478be53702668eabc2c2b6e0e6ba9
ilyt <38846239+ilyt@users.noreply.github.com>
2018-05-21 23:23:52 +0100

    we havent defined these yet

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const { stream, users, channels, console_helper } = require("./source/utilities/
 
 stream.add_stream("main");
 
-channels.add_channel("#osu",        "default osu channel");
-channels.add_channel("#verge",      "main osu!verge channel");
-channels.add_channel("#announce",   "announcements channel", false, true);
-channels.add_channel("#admin",      "admin only channel", true, false);
-channels.add_channel("#std",        "osu!standard channel");
-channels.add_channel("#catch",      "osu!catch channel");
-channels.add_channel("#mania",      "osu!mania channel");
-channels.add_channel("#taiko",      "osu!taiko channel");
+//channels.add_channel("#osu",        "default osu channel");
+//channels.add_channel("#verge",      "main osu!verge channel");
+//channels.add_channel("#announce",   "announcements channel", false, true);
+//channels.add_channel("#admin",      "admin only channel", true, false);
+//channels.add_channel("#std",        "osu!standard channel");
+//channels.add_channel("#catch",      "osu!catch channel");
+//channels.add_channel("#mania",      "osu!mania channel");
+//channels.add_channel("#taiko",      "osu!taiko channel");
 
 users.add_user(new users.user(config.server.bot_name, 1));
 stream.add_user("main", users.get(config.server.bot_name));
